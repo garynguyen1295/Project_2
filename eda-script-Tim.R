4e4ee6a3ab4899295433e137b77ff27c3d3996f7
[GaryThuc Nguyen] <thucnguyen1295@gmail.com>
2016-11-01 23:21:03 -0700

    eda-script, currently on boxplots of Balance with other qualitative variables

--- eda-script-Tim.R
+++ eda-script-Tim.R
@@ -36,7 +36,7 @@ for (index in 1:length(quants)) {
 
 
 ## Make table of frequency and relative frency for qualitative variables
-qualitatives <- c("Gender","Student", "Married")
+qualitatives <- c("Gender","Student", "Married", "Ethnicity")
 
 freq_c <- apply(credit[,qualitatives],2, with)
 
@@ -58,12 +58,43 @@ Married_rel_freq <- prop.table(Married_freq)
 Ethnicity_rel_freq <- prop.table(Ethnicity_freq)
 
 # Bar charts for proportions
-barplot(Gender_rel_freq, ylab = "proportion") 
+barplot(Gender_rel_freq, ylab = "proportion", main = "Relative frequency for Gender") 
 
-barplot(Student_rel_freq, ylab = "proportion")
-barplot(Married_rel_freq, ylab = "proportion")
-barplot(Ethnicity_rel_freq, ylab = "proportion")
+barplot(Student_rel_freq, ylab = "proportion", main = "Relative frequency for Student" )
+barplot(Married_rel_freq, ylab = "proportion", main = "Relative frequency for Married")
+barplot(Ethnicity_rel_freq, ylab = "proportion", main = "Relative frequency for Ethnicity")
+
+# 
+
+#. matrix of correlations among all quantitative variables.
+  
+  # Correlation among numeric variables in 
+   
+credit_quants <- credit[,-(7:10)]
+x <- credit_quants[1:4]
+y <- credit_quants[5:7]
+cor(x,y)
 
 
+## . scatterplot matrix. 
+pairs(credit_quants, main = "Scatterplot matrix among quantitative variables")
+
+#. anova's between Balance and all the qualitative variables (see function aov()).
+
+credit_qualitatives <- credit[,7:10]
+credit_qualitatives #only qualittative variables
+
+fit <- aov(credit$Balance~credit_qualitatives$Gender+credit_qualitatives$Student
+           +credit_qualitatives$Married+credit_qualitatives$Ethnicity)
+summary(fit)
+
+#boxplots conditioning on different variables
+for (index in 1:length(qualitatives)) {
+  qualitative <- qualitatives[index]
+  boxplot(credit$Balance~qualitative, main = paste('Boxplot of Balance 
+                                               conditional on', qualitative))
+}
 
 
+boxplot(credit$Balance~qualitative, main = paste('Boxplot of Balance 
+                                                 conditional on cred'))
