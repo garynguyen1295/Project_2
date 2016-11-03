b0853cd0fde9e33d0e9c2385b77d3305d91d73c1
timothy-park <timothypark713@gmail.com>
2016-11-03 11:12:41 -0700

    preprocessing: commented on each command and used arguments for Makefile

--- code/scripts/preprocessing.R
+++ code/scripts/preprocessing.R
@@ -1,9 +1,22 @@
-credit <- read.csv('http://www-bcf.usc.edu/~gareth/ISL/Credit.csv')
-str(credit)
-credit <- credit[-1]
-str(credit)
+# setting argument for Makefile
+args = commandArgs(trailingOnly = TRUE)
 
-temp_credit <- model.matrix(Balance ~ ., data = credit)
-new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
+# reading credit data set
+raw_credit <- read.csv(file = args[1])
+str(raw_credit)
+
+# taking out index column
+raw_credit <- raw_credit[-1]
+str(raw_credit)
+
+# transforming categoricala variables into dummy variables
+temp_credit <- model.matrix(Balance ~ ., data = raw_credit)
+
+# taking out intercept column and adding Balance columns
+new_credit <- cbind(temp_credit[ ,-1], Balance = raw_credit$Balance)
+
+# standardizine and centering the variables
 scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
-write.csv(scaled_credit, file = "scaled-credit.csv")
+
+# writing the data set to the data directory to be used for regression
+write.csv(scaled_credit, file = "data/scaled-credit.csv")
