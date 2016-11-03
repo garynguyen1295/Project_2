0ad5a5931a1894f8238f14a25e4fbc20c5a9168f
timothy-park <timothypark713@gmail.com>
2016-11-03 12:54:31 -0700

    ridge.R: updated the rscript that performed the ridge analysis and found the best model with the right lambda and coefficients

--- code/scripts/ridge.R
+++ code/scripts/ridge.R
@@ -1,30 +1,62 @@
-# setting argument for Makefile
-args = commandArgs(trailingOnly = TRUE)
+# libraries
+library(glmnet)
 
-# reading scaled credit data set
-scaled_credit <- read.csv(file = args[1])
+# reading in the train and test sets in addition to the full scaled credit data set
+scaled_credit <- read.csv('data/scaled_credit.csv')
 scaled_credit <- scaled_credit[-1]
+train <- read.csv(file = 'data/train_credit.csv')
+train <- train[-1]
+test <- read.csv(file = 'data/test_credit.csv')
+test <- test[-1]
 
-set.seed(400)
-random_300 <- sample(nrow(credit), 300)
-train <- credit[random_300,]
-test <- credit[-random_300,]
-
-lm_ols <- lm(Balance ~ ., data = train)
-
-set.seed(10)
+# setting the grid of lambdas manually
 grid <- 10^seq(10, -2, length = 100)
-ridge <- cv.glmnet(x = as.matrix(train[,-12]), y = as.double(train[,12]), 
-                   intercept = FALSE, standardize = FALSE, alpha = 0, lambda = grid)
-plot(ridge)
 
-save(ridge, file = 'ridgeregressionmodels.RData')
-lambda_opt <- ridge$lambda.min
+# performing a ridge regression onto training set with 10 fold CV
+set.seed(10)
+train_ridge <- cv.glmnet(x = as.matrix(train[,-12]), y = train[,12], 
+                   intercept = FALSE, standardize = FALSE, alpha = 0, 
+                   lambda = grid, nfolds = 10)
 
-png('mse-lamda-ridge.png')
-plot(ridge)
-abline(v = lambda_opt)
+# saving the models to a RData file in the data directory
+save(train_ridge, file = 'data/ridge_models.RData')
+
+# finding the best lambda from the models
+best_lambda <- train_ridge$lambda.min
+
+# plotting the cv erros of the ridge regression and saving the plot as a png image
+png('images/ridge_cv_errors_plot.png')
+plot(train_ridge)
+abline(v = best_lambda)
 dev.off()
 
-ridge <- cv.glmnet(x = as.matrix(test[,-12]), y = as.double(test[,12]), 
-                   intercept = FALSE, standardize = FALSE, alpha = 0, lambda = lambda_opt)
+# predicting the response Balance with the model with best lambda
+ridge_pred <- predict(train_ridge, as.matrix(test[,-12]), s = best_lambda)
+
+# finding the test mse of the optimal model
+ridge_mse <- mean((ridge_pred - test[,12])^2)
+
+# fitting the best ridge model with the optimal lambda
+best_ridge <- glmnet(x = as.matrix(scaled_credit[,-12]), y = scaled_credit[,12], 
+                   intercept = FALSE, standardize = FALSE, alpha = 0, 
+                   lambda = best_lambda)
+
+# outputting the regression coefficients after reformatting the dfC matrix 
+# Intercept is 0 after standardizing/centering
+best_ridge_coef <- coef(best_ridge)
+coef_names <- best_ridge_coef@Dimnames[[1]]
+coef_values <- best_ridge_coef@x
+coef_values <- c(0,coef_values)
+best_ridge_coef <- data.frame('Coefficients' = coef_names, 'Values' = coef_values)
+
+# outputting the best lambda, mse, and coefficients to a textfile
+sink('data/ridge-output.txt')
+cat('Best lambda for ridge regression\n')
+print(best_lambda)
+cat('\nTest mean squared error using the best lambda\n')
+print(ridge_mse)
+cat('\nOfficial coefficients for the optimal model\n')
+print(best_ridge_coef)
+sink()
+
+
