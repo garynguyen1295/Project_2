d585e2ca1d287b3f50645cb6440b759b0940aa4a
timothy-park <timothypark713@gmail.com>
2016-11-03 17:18:52 -0700

    ols_script.R: performed an OLS regression and outputted a text file and RData explaining the model and its coefficients

---
+++ code/scripts/ols_script.R
@@ -0,0 +1,28 @@
+# reading in the training and test data sets
+train <- read.csv(file = 'data/train_credit.csv')
+train <- train[-1]
+test <- read.csv(file = 'data/test_credit.csv')
+test <- test[-1]
+
+# fitting an ols regression onto the training data set
+ols <- lm(Balance ~ ., data = train)
+
+# saving the model to RData output
+save(ols, file = 'data/ols_model.RData')
+
+# finding the coefficients of the fitted model
+ols_coef <- data.frame('Coefficients' = names(ols$coefficients),
+                       'Values' = unname(ols$coefficients))
+
+# predictions of the test using the model fitted onto training
+# test MSE is calculated
+ols_pred <- predict(ols, test[,-12])
+ols_mse <- mean((ols_pred - test[,12])^2)
+
+# creating a textfile outputting the MSE & Coefficients for the fitted model
+sink('data/ols_output.txt')
+cat('Test MSE for OLS model\n')
+print(ols_mse)
+cat('\nCoefficients of OLS Model(Training Set)\n')
+print(ols_coef)
+sink()
