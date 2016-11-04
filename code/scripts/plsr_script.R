776179fe00022983018e0c989680dcb163bd1506
timothy-park <timothypark713@gmail.com>
2016-11-04 13:54:35 -0700

    fixed bugs in lasso and plsr script files and made Makefile compatability

--- code/scripts/plsr_script.R
+++ code/scripts/plsr_script.R
@@ -1,95 +1,49 @@
-# Partial Least Squares Regression
-
-# load required packages
-
+# loading required packages
 library(pls)
 
-source("../functions/regression-functions.R")
-
-# import train and test data
-
-scaled_credit <- read.csv("../../data/scaled-credit.csv", stringsAsFactors=FALSE)
+# importing train and test data
+scaled_credit <- read.csv("data/scaled_credit.csv", stringsAsFactors = FALSE)
 scaled_credit <- scaled_credit[-1]
-
-train <- read.csv("../../data/train_credit.csv", header = TRUE)
-
+train <- read.csv("data/train_credit.csv")
 train <- train[-1]
-
 test <- read.csv(file = 'data/test_credit.csv')
-
 test <- test[-1]
 
-
 # 10-fold cross validation (using training data)
-
 set.seed(10)
+plsr_cv_out <- plsr(Balance ~ ., data = train[,-c(2:3)], validation = "CV")
 
-plsr_cv_out <- plsr(Balance ~., data = train[,-c(2:3)], validation = "CV")
-
-
-
-# best model
-
+# Best model
 plsr_PRESS <- min(plsr_cv_out$validation$PRESS)
-
 plsr_ncomp_all <- plsr_cv_out$validation$PRESS
-
 plsr_ncomp_use <- which(plsr_ncomp_all[1,]==plsr_PRESS)
-
 plsr_ncomp <- plsr_ncomp_use[[1]]
 
-
-
-
-
 #plot cross-validation errors
-
-png("../../images/plsr-cv.png")
-
+png("images/plsr-cv.png")
 validationplot(plsr_cv_out, val.type = "MSEP")
-
 dev.off()
 
-
-
 # compute test mse using test data
-
 x_test = as.matrix(test[,-c(2:3,12)])
-
 y_test = test[,12]
-
 plsr_pred = predict(plsr_cv_out, newx = x_test, ncomp = plsr_ncomp)
-
-plsr_test_mse = mse(plsr_pred,y_test)
-
-
+plsr_test_mse = mean((plsr_pred - y_test)^2)
 
 # full model
-
 plsr_full <- plsr(Balance ~., data = test[,-c(2:3)], ncomp = plsr_ncomp)
-summaey(plsr_full)
-
+summary(plsr_full)
 
 # save objects to file
-
-save(plsr_cv_out,plsr_PRESS, plsr_ncomp, plsr_test_mse, plsr_full, file = "../../data/plsr-cv.RData")
+save(plsr_cv_out, file = "data/plsr_models.RData")
 
 #Save output
-sink(file = "../../data/pls/pls-output.txt")
-
+sink(file = "data/pls_output.txt")
 writeLines("Partial Least Squares")
-
 writeLines("Best number of components")
-
-pls_ncomp_use
-
+plsr_ncomp
 writeLines("Summary of pls model")
-
-summary(pls_full)
-
+summary(plsr_full)
 writeLines("Test MSE")
-
-pls_test_mse
-
+plsr_test_mse
 sink()
-
